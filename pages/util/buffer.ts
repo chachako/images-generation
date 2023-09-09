7eb04ed1e738be41f564563d9216d3799dbe0fac
茶茶 <chachakko@outlook.com>
2023-09-09 12:05:33 +0800

    fix: replace `fs` with `fetch`

--- pages/util/buffer.ts
+++ pages/util/buffer.ts
@@ -1,20 +1,11 @@
-import fs from 'fs'
-import { DEPLOYMENT_URL, RUNNING_LOCAL } from './env.ts'
+import { DEPLOYMENT_URL } from './env.ts'
 
 export async function getFileBuffer(file: string) {
-  if (RUNNING_LOCAL) {
-    const res = await fetch(`${DEPLOYMENT_URL}/${file}`)
-    return await res.arrayBuffer()
-  } else {
-    return Promise.resolve(fs.readFileSync(file))
-  }
+  const res = await fetch(`${DEPLOYMENT_URL}/${file}`)
+  return await res.arrayBuffer()
 }
 
 export async function getFileString(file: string) {
-  if (RUNNING_LOCAL) {
-    const res = await fetch(`${DEPLOYMENT_URL}/${file}`)
-    return await res.text()
-  } else {
-    return Promise.resolve(fs.readFileSync(file, 'utf8'))
-  }
+  const res = await fetch(`${DEPLOYMENT_URL}/${file}`)
+  return await res.text()
 }
